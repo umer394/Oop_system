3c18bd07f62fdcec1b3855933ffd98a17bc2d9ec
umer <umershhzad7@gmail.com>
2024-08-10 06:20:06 -0700

    pushing oop project

---
+++ index.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Person {
+    personality;
+    constructor() {
+        this.personality = "mystery";
+    }
+    askQuestion(answer) {
+        if (answer === 1) {
+            this.personality = "Extrovert";
+        }
+        else if (answer == 2) {
+            this.personality = "Introvert";
+        }
+        else {
+            this.personality = "still a mystery";
+        }
+    }
+    getPersonality() {
+        return this.personality;
+    }
+}
+class Student extends Person {
+    _name;
+    constructor() {
+        super();
+        this._name = "";
+    }
+    get Name() {
+        return this._name;
+    }
+    set Name(value) {
+        this._name = value;
+    }
+}
+try {
+    let input = await inquirer.prompt({
+        name: "userInput",
+        type: "number",
+        message: "Type 1 if youu want to talk with others and type 2 if you would rather keep  to yourself"
+    });
+    let myPerson = new Person();
+    myPerson.askQuestion(parseInt(input.userInput));
+    console.log("You are: " + myPerson.getPersonality());
+    let inputQues = await inquirer.prompt({
+        name: "userQues",
+        type: "input",
+        message: "What is your name?"
+    });
+    let myStudent = new Student();
+    myStudent.Name = inputQues.userQues;
+    console.log(`Your name is ${myStudent.Name} and your personlaity type is ${myPerson.getPersonality()}`);
+}
+catch {
+    console.log("Please enter an integer value");
+}
